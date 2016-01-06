56ab8c6e59d50ae6e659fafcdb1176dc74bfe526
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-01-05 21:58:06 -0800

    Fix bugs, add color

--- project01/project01.asm
+++ project01/project01.asm
@@ -66,171 +66,208 @@ INCLUDE Irvine32.inc
 .code
 main PROC
 
+; Change colors to white on black - default
+	mov		eax,white (black * 16)	
+	call	SetTextColor
+
 ; introduce program + programmer
-	mov	edx, OFFSET programTitle
+	mov		edx, OFFSET programTitle
 	call	WriteString
-	mov	edx, OFFSET myName
+	mov		edx, OFFSET myName
 	call	WriteString
 	call	CrLf
 	
 	; Extra Credit 1 Implemented message
-	mov	edx, OFFSET ecIntro_1
+	mov		edx, OFFSET ecIntro_1
 	call	WriteString
 	call	CrLf
 	
 	; Extra Credit 2 Implemented message
-	mov	edx, OFFSET ecIntro_2
+	mov		edx, OFFSET ecIntro_2
 	call	WriteString
 	call	CrLf
 	
 	; Extra Credit 3 Implemented message
-;	mov	edx, OFFSET ecIntro_3
+;	mov		edx, OFFSET ecIntro_3
 ;	call	WriteString
 ;	call	CrLf
 
 
+
 MAINLOOP:
+
+	call	CrLf
+	call	CrLf
 
 ; get two values, value_1 and value_2, from the user
 
 	; print instructions
-	mov	edx, OFFSET instruct_1
-	call	WriteString
-	call	CrLf
-	mov	edx, OFFSET instruct_2
-	call	WriteString
-	call	CrLf
+		mov		edx, OFFSET instruct_1
+		call	WriteString
+		call	CrLf
+		mov		edx, OFFSET instruct_2
+		call	WriteString
+		call	CrLf
 
 	; print prompt for value_1
-	mov	edx, OFFSET promptVal_1
-	call	WriteString
-	call	CrLf
+		mov		edx, OFFSET promptVal_1
+		call	WriteString
+		call	CrLf
 
 	; get input value_1
-	call	ReadDec
-	mov value_1, eax
+		call	ReadDec
+		mov		value_1, eax
 
 	; print prompt for value_2
-	mov	edx, OFFSET promptVal_2
-	call	WriteString
-	call	CrLf
+		mov		edx, OFFSET promptVal_2
+		call	WriteString
+		call	CrLf
 
 	; get input value_2
-	call	ReadDec
-	mov 	value_2, eax
+		call	ReadDec
+		mov 	value_2, eax
 		
+
 ; **EC: 2. Validate the second number to be less than the first.
 	; if value_2 > value_1, Jump to QUITPROMPT that controls main program loop
-	mov 	eax, value_2
-	cmp	eax, value_1
-	jg	VALUEERROR
+		mov 	eax, value_2
+		cmp		eax, value_1
+		jg		INVALID
 	
+
 ; calculate the required values
 
 	; Calculate the sum
-	mov 	eax,value_1
-	add 	eax,value_2
-	mov 	sum,eax
+		mov 	eax,value_1
+		add 	eax,value_2
+		mov 	sum,eax
 
 	; Calculate the difference
-	mov 	eax,value_1
-	sub 	eax,value_2
-	mov 	difference,eax
+		mov 	eax,value_1
+		sub 	eax,value_2
+		mov 	difference,eax
 
 	; Calculate the product
-	mov 	eax,value_1
-	mul 	value_2
-	mov 	product,eax
+		mov 	eax,value_1
+		mul 	value_2
+		mov 	product,eax
 
 	; Calculate the integer quotient (eax is divsor, ebx is dividend)
-	mov 	eax, value_1	
-	cdq	               	; zero-extends eax into edx
-	mov 	ebx, value_2
-	div 	ebx
-	mov 	quotient, eax
-	mov 	remainder, edx
+		mov		eax, value_1	
+		cdq	               	; zero-extends eax into edx
+		mov 	ebx, value_2
+		div 	ebx
+		mov 	quotient, eax
+		mov 	remainder, edx
 
 ; display the results
+
 	; Print sum to screen with message
-	mov	edx, OFFSET sumMsg
-	call	WriteString
-	mov	eax, sum
-	call	WriteDec
-	call	CrLf
+		mov		edx, OFFSET sumMsg
+		call	WriteString
+		mov		eax, sum
+		call	WriteDec
+		call	CrLf
 
 	; Print difference to screen with message
-	mov	edx, OFFSET differenceMsg
-	call	WriteString
-	mov	eax, difference
-	call	WriteDec
-	call	CrLf
+		mov		edx, OFFSET differenceMsg
+		call	WriteString
+		mov		eax, difference
+		call	WriteDec
+		call	CrLf
 
 	; Print product to screen with message
-	mov	edx, OFFSET productMsg
-	call	WriteString
-	mov	eax, product
-	call	WriteDec
-	call	CrLf
+		mov		edx, OFFSET productMsg
+		call	WriteString
+		mov		eax, product
+		call	WriteDec
+		call	CrLf
 
 	; Print quotient to screen with message
-	mov	edx, OFFSET quotientMsg
-	call	WriteString
-	mov	eax, quotient
-	call	WriteDec
-	call	CrLf
+		mov		edx, OFFSET quotientMsg
+		call	WriteString
+		mov		eax, quotient
+		call	WriteDec
+		call	CrLf
 
 	; Print remainder to screen with message
-	mov	edx, OFFSET remainderMsg
-	call	WriteString
-	mov	eax, remainder
-	call	WriteDec
-	call	CrLf
+		mov		edx, OFFSET remainderMsg
+		call	WriteString
+		mov		eax, remainder
+		call	WriteDec
+		call	CrLf
 		
 
 ; **EC: 1. Repeat until the user chooses to quit.
 
 QUITPROMPT:
 	; Prompt user if they wish to (Q)uit or (C)ontinue 
-	mov	edx, OFFSET repeatMsg
-	call	WriteString
-	call 	CrLf
+		mov		edx, OFFSET repeatMsg
+		call	WriteString
+		call 	CrLf
 	
-	; Get response, convert to uppercase by clearing bit 5 (Page 192 of Irvine)
-	call 	ReadChar
-	mov 	response, al
-	and 	response, 11011111b	
+	; Get loopResponse, convert to uppercase by clearing bit 5 (Page 192 of Irvine)
+		call 	ReadChar
+		mov 	loopResponse, al
+		and 	loopResponse, 11011111b	
 	
-	; Compare response to continue ('C') and jump to MAINLOOP label if matches (Page 197+ of Irvine)
-	cmp	response,continue	; ZF=1,CF=0 if response==source with unsigned operands
-	jz	MAINLOOP
+	; Compare loopResponse to continue ('C') and jump to MAINLOOP label if matches (Page 197+ of Irvine)
+		mov		al,loopResponse
+		cmp		al,continue	; ZF=1,CF=0 if loopResponse==source with unsigned operands
+		jz		MAINLOOP
 	
-	; Compare response to quit ('Q') and jump to GOODBYE label if matches (Page 197+ of Irvine)
-	cmp	response,quit		
-	jz	GOODBYE
+	; Compare loopResponse to quit ('Q') and jump to GOODBYE label if matches (Page 197+ of Irvine)
+		mov		al,loopResponse
+		cmp		al,quit		
+		jz		GOODBYE
 		
-	; Print error message if Q or C is not entered and repeat prompt
-	mov	edx, OFFSET promptError
-	call	WriteString
-	call 	CrLf
-	jmp	QUITPROMPT
 
-; Jump here from MAINLOOP if value_2 > value_1
-VALUEERROR:
+	; Change colors to white on red - error
+		mov		eax,white (red * 16)	
+		call	SetTextColor
+
+	; Print error message if Q or C is not entered and repeat prompt
+		mov		edx, OFFSET promptError
+		call	WriteString
+		call 	CrLf
+
+	; Change colors to white on black - default
+		mov		eax,white (black * 16)	
+		call	SetTextColor
+
+	jmp		QUITPROMPT
+
+
+
+INVALID:	; Jump here from MAINLOOP if value_2 > value_1
+
+	; Change colors to white on red - error
+		mov		eax,white (red * 16)	
+		call	SetTextColor
+	
 	; Print error message that value_2 must be less than value_1, jump to reprompt
-	mov	edx, OFFSET valueError
-	call	WriteString
-	call	CrLf
-	jmp	MAINLOOP
+		mov		edx, OFFSET valueError
+		call	WriteString
+		call	CrLf
+
+	; Change colors to white on black - default
+		mov		eax,white (black * 16)	
+		call	SetTextColor
+		jmp		MAINLOOP
+
+
 
 ; say goodbye
+
 GOODBYE:
 	; print exitMsg
-	mov	edx, OFFSET exitMsg
-	call 	WriteString
-	call 	CrLf
+		mov		edx, OFFSET exitMsg
+		call 	WriteString
+		call 	CrLf
 		
 	; exit to operating system
-	exit	
+		exit	
+
 main ENDP
 
 END main
